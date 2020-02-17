09614e507931a3f50ff37ee31aa8c16d7154504c
Кочкин Александр <Alexander.Kochkin@artezio.com>
2020-02-17 12:32:40 +0400

    Added stub.

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import HomePage from './home-page';
+
+ReactDOM.render(<HomePage />, document.getElementById('root'));
